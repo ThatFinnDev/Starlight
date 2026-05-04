874aab4305652fba7eba63ab24cb6eafd3fe969d
ThatFinn <finn@thatfinn.dev>
2026-05-04 21:56:54 +0200

    Fix broken Harmony Patch

--- Essentials/Patches/InGame/BaseUIClosePatch.cs
+++ Essentials/Patches/InGame/BaseUIClosePatch.cs
@@ -2,7 +2,7 @@ using Il2CppMonomiPark.SlimeRancher.UI;
 
 namespace Starlight.Patches.InGame;
 
-[HarmonyPatch(typeof(BaseUI), nameof(BaseUI.Close))]
+[HarmonyPatch(typeof(BaseUI), nameof(BaseUI.Close), [])]
 internal static class BaseUIClosePatch
 {
     internal static void Prefix()
